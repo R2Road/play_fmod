c22a5c65b0e26361a17f03daf86c7a82ac0cf48f
r2road <studior2road@gmail.com>
2021-05-28 03:22:34 +0900

    update : fmod_test - PlayAndCallback

--- fmod_playground/src/fmod_2_test.cpp
+++ fmod_playground/src/fmod_2_test.cpp
@@ -262,25 +262,10 @@ namespace fmod_test
 			// Preload Audio + Setup
 			//
 			FMOD::Sound* fmod_sound = nullptr;
-			FMOD::Sound* fmod_current_sound = nullptr;
 			{
 				// Preload Audio
 				fmod_result = fmod_system->createStream( "resources/TremLoadingloopl.wav", FMOD_LOOP_OFF | FMOD_2D, 0, &fmod_sound );
 				r2_fmod_util::ERROR_CHECK( fmod_result );
-
-				int sub_sound_count = 0;
-				fmod_result = fmod_sound->getNumSubSounds( &sub_sound_count );
-				r2_fmod_util::ERROR_CHECK( fmod_result );
-
-				if( 0 < sub_sound_count )
-				{
-					fmod_sound->getSubSound( 0, &fmod_current_sound );
-					r2_fmod_util::ERROR_CHECK( fmod_result );
-				}
-				else
-				{
-					fmod_current_sound = fmod_sound;
-				}
 			}
 
 			//
@@ -299,7 +284,7 @@ namespace fmod_test
 						switch( _getch() )
 						{
 						case '1':
-							fmod_result = fmod_system->playSound( fmod_current_sound, 0, false, &fmod_channel );
+							fmod_result = fmod_system->playSound( fmod_sound, 0, false, &fmod_channel );
 							r2_fmod_util::ERROR_CHECK( fmod_result );
 
 							fmod_channel->setCallback( TempCallback );
