3ddf03d695e78e288fc612fad6e444cd9761c0c6
r2road <studior2road@gmail.com>
2021-05-29 04:59:11 +0900

    fix : channel callback need F_CALLBACK

--- fmod_playground/src/fmod_play_test.cpp
+++ fmod_playground/src/fmod_play_test.cpp
@@ -237,9 +237,20 @@ namespace fmod_play_test
 
 
 
-	FMOD_RESULT TempCallback( FMOD_CHANNELCONTROL* /*channelcontrol*/, FMOD_CHANNELCONTROL_TYPE /*controltype*/, FMOD_CHANNELCONTROL_CALLBACK_TYPE /*callbacktype*/, void* /*commanddata1*/, void* /*commanddata2*/ )
+	FMOD_RESULT F_CALLBACK TempCallback( FMOD_CHANNELCONTROL* channelcontrol, FMOD_CHANNELCONTROL_TYPE controltype, FMOD_CHANNELCONTROL_CALLBACK_TYPE /*callbacktype*/, void* /*commanddata1*/, void* /*commanddata2*/ )
 	{
 		std::cout << "Call - TempCallback" << r2::linefeed;
+
+		if( controltype == FMOD_CHANNELCONTROL_TYPE::FMOD_CHANNELCONTROL_CHANNEL )
+		{
+			FMOD::Channel *channel = ( FMOD::Channel * )channelcontrol;
+			// Channel specific functions here...
+		}
+		else
+		{
+			FMOD::ChannelGroup *group = ( FMOD::ChannelGroup * )channelcontrol;
+			// ChannelGroup specific functions here...
+		}
 
 		system( "pause" );
 
