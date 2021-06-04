232b0fee453c352f706a26e09d6fd4b4db84f44a
r2road <studior2road@gmail.com>
2021-06-05 00:21:11 +0900

    update function : r2_fmod_util - ERROR_CHECK

--- fmod_playground/src/utility/r2_fmod_util.cpp
+++ fmod_playground/src/utility/r2_fmod_util.cpp
@@ -201,7 +201,7 @@ namespace r2_fmod_util
 
 	void ERROR_CHECK( const FMOD_RESULT fmod_result )
 	{
-		if( FMOD_OK != fmod_result )
+		if( ( FMOD_OK != fmod_result ) && ( fmod_result != FMOD_ERR_INVALID_HANDLE ) && ( fmod_result != FMOD_ERR_CHANNEL_STOLEN ) )
 		{
 			R2ASSERT( false, FMOD_ErrorString( fmod_result ) );
 		}
