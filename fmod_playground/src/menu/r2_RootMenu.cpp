0b5fd1b9922d55df0972282748a1ae3d92941915
r2road <studior2road@gmail.com>
2021-06-11 20:17:35 +0900

    ...

--- fmod_playground/src/menu/r2_RootMenu.cpp
+++ fmod_playground/src/menu/r2_RootMenu.cpp
@@ -27,7 +27,9 @@ namespace r2
 			ret->AddChild( '1', fmod_basic_test::SystemCreateAndRelease::GetInstance() );
 			ret->AddChild( '2', fmod_basic_test::VersionCheck::GetInstance() );
 
+
 			ret->AddLineFeed();
+
 
 			ret->AddChild( '3', fmod_load_test::Basic::GetInstance() );
 			ret->AddChild( '4', fmod_play_test::PlaySound::GetInstance() );
@@ -36,32 +38,44 @@ namespace r2
 			ret->AddChild( '7', fmod_play_test::VolumeControl::GetInstance() );
 			ret->AddChild( '8', fmod_play_test::PositionControl::GetInstance() );
 
+
 			ret->AddLineFeed();
+
 
 			ret->AddChild( '9', fmod_load_test::Memory::GetInstance() );
 
+
 			ret->AddLineFeed();
+
 
 			ret->AddChild( 'q', fmod_effect_test::Echo::GetInstance() );
 			ret->AddChild( 'w', fmod_effect_test::Fade::GetInstance() );
 			ret->AddChild( 'e', fmod_effect_test::FadeStartFadeEnd::GetInstance() );
 			ret->AddChild( 'r', fmod_effect_test::PitchControl::GetInstance() );
 
+
 			ret->AddLineFeed();
+
 
 			ret->AddChild( 'a', fmod_channel_group_test::Basic::GetInstance() );
 			ret->AddChild( 's', fmod_channel_group_test::Group::GetInstance() );
 			ret->AddChild( 'd', fmod_channel_group_test::Volume::GetInstance() );
 
+
 			ret->AddLineFeed();
+
 
 			ret->AddChild( 'z', fmod_sound_analysis_test::Frequency::GetInstance() );
 
+
 			ret->AddSplit();
+
 
 			ret->AddChild( 32, etc_test::Test1::GetInstance() );
 
+
 			ret->AddSplit();
+
 
 			ret->AddChild(
 				27
