b19daac2756f92d606b2462971ed5e60908da234
r2road <studior2road@gmail.com>
2022-03-29 13:07:58 +0900

    ...

--- fmod_playground/src/r2/r2_Assert.h
+++ fmod_playground/src/r2/r2_Assert.h
@@ -9,9 +9,9 @@
 	#define R2_DEBUG 1
 
 	#if !defined( R2_DEBUG ) || R2_DEBUG == 0
-		#define R2ASSERT( cond, msg )     do {} while ( 0 )
+		#define R2ASSERT_2( cond, msg )     do {} while ( 0 )
 	#else
-		#define R2ASSERT( cond, msg )     do { std::cerr << msg; assert( cond ); } while ( 0 )
+		#define R2ASSERT_2( cond, msg )     do { std::cerr << msg; assert( cond ); } while ( 0 )
 	#endif
 
 #endif
