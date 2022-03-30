3eb86d9f5601ced166d7097b390061caaa0a181e
r2road <studior2road@gmail.com>
2022-03-30 23:39:55 +0900

    apply inspector

--- fmod_playground/src/test_fmod/item/fmod_load_test.cpp
+++ fmod_playground/src/test_fmod/item/fmod_load_test.cpp
@@ -6,6 +6,7 @@
 #include "fmod.hpp"
 #include "fmod_errors.h"
 
+#include "r2/r2_Inspector.h"
 #include "r2cm/r2cm_eTestEndAction.h"
 
 #include "r2/r2_Assert.h"
@@ -27,29 +28,31 @@ namespace fmod_load_test
 		{
 			std::cout << "# " << GetInstance().GetTitleFunction()( ) << " #" << r2::linefeed;
 
-			FMOD::System* fmod_system = nullptr;
-			FMOD_RESULT fmod_result = FMOD_RESULT::FMOD_OK;
+			std::cout << r2::split;
 
-			r2_fmod_util::CreateSystem( &fmod_system );
+			DECLARATION_SUB( FMOD::System* fmod_system = nullptr );
+			DECLARATION_SUB( FMOD_RESULT fmod_result = FMOD_RESULT::FMOD_OK );
+			PROCESS_SUB( r2_fmod_util::CreateSystem( &fmod_system ) );
+
+			std::cout << r2::split;
+
+			DECLARATION_MAIN( FMOD::Sound* fmod_sound = nullptr );
 
 			std::cout << r2::split;
 
 			//
 			// Audio Load
 			//
-			FMOD::Sound* fmod_sound = nullptr;
 			{
-				// Load
-				fmod_result = fmod_system->createSound( "resources/TremLoadingloopl.wav", FMOD_DEFAULT, 0, &fmod_sound );
-				r2_fmod_util::ERROR_CHECK( fmod_result );
+				std::cout << r2::tab << "+ Load" << r2::linefeed2;
+				PROCESS_MAIN( fmod_result = fmod_system->createSound( "resources/TremLoadingloopl.wav", FMOD_DEFAULT, 0, &fmod_sound ) );
+				PROCESS_MAIN( r2_fmod_util::ERROR_CHECK( fmod_result ) );
 
-				// Setup
-				fmod_result = fmod_sound->setMode( FMOD_LOOP_OFF );
-				r2_fmod_util::ERROR_CHECK( fmod_result );
+				std::cout << r2::linefeed;
 
-				std::cout << r2::tab << "+ Load" << r2::linefeed << r2::linefeed;
-				std::cout << r2::tab2 << "FMOD::Sound* fmod_sound = nullptr;" << r2::linefeed;
-				std::cout << r2::tab2 << "fmod_result = fmod_system->createSound( \"resources/TremLoadingloopl.wav\", FMOD_DEFAULT, 0, &fmod_sound );" << r2::linefeed;
+				std::cout << r2::tab << "+ Setup" << r2::linefeed2;
+				PROCESS_MAIN( fmod_result = fmod_sound->setMode( FMOD_LOOP_OFF ) );
+				PROCESS_MAIN( r2_fmod_util::ERROR_CHECK( fmod_result ) );
 			}
 
 			std::cout << r2::split;
@@ -58,16 +61,15 @@ namespace fmod_load_test
 			// Audio Release
 			//
 			{
-				fmod_result = fmod_sound->release();
-				r2_fmod_util::ERROR_CHECK( fmod_result );
-
-				std::cout << r2::tab << "+ Release" << r2::linefeed << r2::linefeed;
-				std::cout << r2::tab2 << "fmod_result = fmod_sound->release();" << r2::linefeed;
+				PROCESS_MAIN( fmod_result = fmod_sound->release() );
+				PROCESS_MAIN( r2_fmod_util::ERROR_CHECK( fmod_result ) );
 			}
 
 			std::cout << r2::split;
 
-			r2_fmod_util::ReleaseSystem( &fmod_system );
+			PROCESS_SUB( r2_fmod_util::ReleaseSystem( &fmod_system ) );
+
+			std::cout << r2::split;
 
 			return r2cm::eTestEndAction::Pause;
 		};
