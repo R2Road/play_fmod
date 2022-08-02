ad93a918ddc1f62d09f5d77ad0f3b105707e0e30
r2road <studior2road@gmail.com>
2022-08-02 16:06:25 +0900

    apply r2cm::inspector

--- fmod_playground/src/test_fmod/item/fmod_channel_group_test.cpp
+++ fmod_playground/src/test_fmod/item/fmod_channel_group_test.cpp
@@ -68,27 +68,32 @@ namespace fmod_channel_group_test
 		{
 			std::cout << "# " << GetInstance().GetTitleFunction()( ) << " #" << r2::linefeed;
 
-			std::cout << r2::split;
-
+			//
+			//
+			//
 			FMOD::System* fmod_system = nullptr;
 			FMOD_RESULT fmod_result = FMOD_RESULT::FMOD_OK;
-
-			FMOD::ChannelGroup* fmod_master_channelgroup = nullptr;
-
-			FMOD::ChannelGroup* groupA = nullptr;
-			FMOD::ChannelGroup* groupB = nullptr;
-
 			r2_fmod_util::CreateSystem( &fmod_system );
+
+			std::cout << r2::split;
+
+			//
+			//
+			//
+			DECLARATION_MAIN( FMOD::ChannelGroup* fmod_master_channelgroup = nullptr );
+			DECLARATION_MAIN( FMOD::ChannelGroup* groupA = nullptr );
+			DECLARATION_MAIN( FMOD::ChannelGroup* groupB = nullptr );
+
+			std::cout << r2::split;
 
 			//
 			// Master Channel Group
 			//
 			{
-				fmod_result = fmod_system->getMasterChannelGroup( &fmod_master_channelgroup );
-				r2_fmod_util::ERROR_CHECK( fmod_result );
+				std::cout << r2::tab << "+ Get Master Channel Group" << r2::linefeed2;
 
-				std::cout << r2::tab << "+ Get Master Channel Group" << r2::linefeed << r2::linefeed;
-				std::cout << r2::tab2 << "fmod_result = fmod_system->getMasterChannelGroup( &fmod_master_channelgroup );" << r2::linefeed;
+				PROCESS_MAIN( fmod_result = fmod_system->getMasterChannelGroup( &fmod_master_channelgroup ) );
+				r2_fmod_util::ERROR_CHECK( fmod_result );
 			}
 
 			std::cout << r2::split;
@@ -97,15 +102,13 @@ namespace fmod_channel_group_test
 			// Create Channel Group
 			//
 			{
-				fmod_result = fmod_system->createChannelGroup( "Group A", &groupA );
+				std::cout << r2::tab << "+ Create Channel Group" << r2::linefeed2;
+
+				PROCESS_MAIN( fmod_result = fmod_system->createChannelGroup( "Group A", &groupA ) );
 				r2_fmod_util::ERROR_CHECK( fmod_result );
 
-				fmod_result = fmod_system->createChannelGroup( "Group B", &groupB );
+				PROCESS_MAIN( fmod_result = fmod_system->createChannelGroup( "Group B", &groupB ) );
 				r2_fmod_util::ERROR_CHECK( fmod_result );
-
-				std::cout << r2::tab << "+ Create Channel Group" << r2::linefeed << r2::linefeed;
-				std::cout << r2::tab2 << "fmod_system->createChannelGroup( \"Group A\", &groupA )" << r2::linefeed;
-				std::cout << r2::tab2 << "fmod_system->createChannelGroup( \"Group A\", &groupB )" << r2::linefeed;
 			}
 
 			std::cout << r2::split;
@@ -114,15 +117,13 @@ namespace fmod_channel_group_test
 			// Add Channel Group
 			//
 			{
-				fmod_result = fmod_master_channelgroup->addGroup( groupA );
+				std::cout << r2::tab << "+ Add Channel Group" << r2::linefeed2;
+
+				PROCESS_MAIN( fmod_result = fmod_master_channelgroup->addGroup( groupA ) );
 				r2_fmod_util::ERROR_CHECK( fmod_result );
 
-				fmod_result = fmod_master_channelgroup->addGroup( groupB );
+				PROCESS_MAIN( fmod_result = fmod_master_channelgroup->addGroup( groupB ) );
 				r2_fmod_util::ERROR_CHECK( fmod_result );
-
-				std::cout << r2::tab << "+ Add Channel Group" << r2::linefeed << r2::linefeed;
-				std::cout << r2::tab2 << "fmod_result = fmod_master_channelgroup->addGroup( groupA );" << r2::linefeed;
-				std::cout << r2::tab2 << "fmod_result = fmod_master_channelgroup->addGroup( groupB );" << r2::linefeed;
 			}
 
 			std::cout << r2::split;
@@ -131,17 +132,18 @@ namespace fmod_channel_group_test
 			// Release Channel Group
 			//
 			{
-				fmod_result = groupA->release();
+				std::cout << r2::tab << "+ Release Channel Group" << r2::linefeed2;
+
+				PROCESS_MAIN( fmod_result = groupA->release() );
 				r2_fmod_util::ERROR_CHECK( fmod_result );
 
-				fmod_result = groupB->release();
+				PROCESS_MAIN( fmod_result = groupB->release() );
 				r2_fmod_util::ERROR_CHECK( fmod_result );
-
-				std::cout << r2::tab << "+ Release Channel Group" << r2::linefeed << r2::linefeed;
-				std::cout << r2::tab2 << "fmod_result = groupA->release();" << r2::linefeed;
-				std::cout << r2::tab2 << "fmod_result = groupB->release();" << r2::linefeed;
 			}
 
+			//
+			//
+			//
 			r2_fmod_util::ReleaseSystem( &fmod_system );
 
 			std::cout << r2::split;
